32c5491f8d3744af1bf8b106e34d6d2f754c7a38
Alexander Bantyev <balsoft@balsoft.ru>
2022-05-29 02:14:37 +0400

    ucg: stdenv -> gcc10Stdenv

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6655,7 +6655,7 @@ with pkgs;
 
   robodoc = callPackage ../tools/text/robodoc { };
 
-  ucg = callPackage ../tools/text/ucg { };
+  ucg = callPackage ../tools/text/ucg { stdenv = gcc10StdenvCompat; };
 
   grive2 = callPackage ../tools/filesystems/grive2 { };
 

7c3f02e656bf50f63c099e177835f5b911cd6fe4
Alexander Bantyev <balsoft@balsoft.ru>
2022-05-29 02:12:20 +0400

    fluxus: stdenv -> gcc10Stdenv

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -26217,7 +26217,7 @@ with pkgs;
 
   flrig = callPackage ../applications/radio/flrig { };
 
-  fluxus = callPackage ../applications/graphics/fluxus { };
+  fluxus = callPackage ../applications/graphics/fluxus { stdenv = gcc10StdenvCompat; };
 
   flwrap = callPackage ../applications/radio/flwrap { };
 

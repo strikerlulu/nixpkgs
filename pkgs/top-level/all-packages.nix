56e762f1db0e4c1360434f05cfe527662e8678e5
Alexander Bantyev <balsoft@balsoft.ru>
2022-05-29 02:14:02 +0400

    pktgen: stdenv -> gcc10Stdenv

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23634,7 +23634,7 @@ with pkgs;
 
   pipework = callPackage ../os-specific/linux/pipework { };
 
-  pktgen = callPackage ../os-specific/linux/pktgen { };
+  pktgen = callPackage ../os-specific/linux/pktgen { stdenv = gcc10StdenvCompat; };
 
   plymouth = callPackage ../os-specific/linux/plymouth { };
 
